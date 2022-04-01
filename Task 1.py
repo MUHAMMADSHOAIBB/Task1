0a90663cfb2812e6a81d2c5ea3a36134c1135ec4
MUHAMMADSHOAIBB <99481729+MUHAMMADSHOAIBB@users.noreply.github.com>
2022-04-01 10:41:20 +0500

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,20 @@
+"""
+write down the integers,floating,complex and string
+"""
+x1=55
+y1=69.5
+x2=70
+y2=99.5
+x3=46
+x4=66
+y4=65.5
+x5=100
+z1="my name is shoaib"
+z2="Im a student of CS"
+z3= "abcdesfsghsjk"
+a= 9+8j
+b= 7j
+c= 9j
+d=45-10j
+print(type(x1),type(y1),type(x2),type(y2),type(x3),type(x4),type(y4),type(x5),type(z1),type(z2),type(z3))
+print(type(a),type(b),type(c),type(d))
